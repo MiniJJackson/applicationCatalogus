50b084276179e5cdc200188f15db4c9655cd6895
Jenna Van De Vyver <r0889235@student.thomasmore.be>
2024-01-06 23:56:34 +0100

    API NOT WORKING! SyntaxError: JSON Parse error: Unexpected character: < grrr

---
+++ components/ClothingItem.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
+const ClothingItem = props => {
+  return (
+    <TouchableOpacity activeOpacity={0.5} onPress={() => props.onSelectedClothing(props.id)}>
+      <View style={styles.clothingItem}>
+        <Image
+          style={styles.banner}
+          source={{
+            uri: props.banner
+          }}
+        />
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.intro}>{props.intro}</Text>
+      </View>
+    </TouchableOpacity >
+
+  );
+}
+
+const styles = StyleSheet.create({
+  clothingItem: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: "#f1f1f1",
+    borderRadius: 5,
+    shadowRadius: 1,
+    shadowColor: "#C6DDF2",
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+  },
+  banner: {
+    height: 100
+  },
+  title: {
+    fontWeight: "bold",
+    color: "#96C6F3",
+    fontSize: 16,
+    marginTop: 12,
+    marginBottom: 12,
+    textTransform: "uppercase"
+  },
+  intro: {
+    marginBottom: 8
+  }
+});
+export default ClothingItem;
--- screens/ClothingScreen.js
+++ screens/ClothingScreen.js
@@ -1,13 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
-const CothingScreen = ({ navigation }) => {
+import { StyleSheet, Text, View, Button, FlatList, Platform} from 'react-native';
+
+import ClothingItem from '../components/ClothingItem';
+
+const ClothingScreen = ({ navigation }) => {
+  const [clothing, setClothing] = useState([]); /* om status variabelen bij te houden*/
+
+  const getAllClothing = async () => {
+    try{
+      //10.0.2.2:52989 -> op laptop
+      //10.0.2.2:32775 -> op laptop thuis
+      //http://website-catalogue.ddev.site -> op server (ddev)
+
+      let url;
+
+      if(Platform.OS == 'android'){
+        url = "http://10.0.2.2:32775/api/clothing/";
+      }else{
+          url = "http://website-catalogue.ddev.site/api/clothing/";
+      }
+
+      const response = await fetch(url, { "method": "GET" });
+      const json = await response.json();
+      
+      console.log(json.items);
+
+      setClothing(json.items);
+
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(() => { // Helps you syncronize component with extrnal system - server
+    getAllClothing();
+  }, []);
+
+  //* RETURN *//
+
   return (
     <View style={styles.container}>
-      <Text>CLOTHING - SCREEN</Text>
-      <Button title="Go to details" onPress={() => navigation.navigate('Details')}/>
+      <Text style={styles.title}>All Clothing</Text>
+
+      {/*VIEW FOR BUTTON TOP*/}
+      <View style={styles.buttons}>
       <Button title="back to start" onPress={() => navigation.navigate('Welcome')}/>
-      <StatusBar style="auto" />
+      </View>
+      
+      {/*FLATLIST FOR LIST*/}
+
+      <FlatList
+        style={styles.list}
+        data={clothing}
+        keyExtractor={item => item.id}//gebruik id als key voor flatlist
+        renderItem={({item})=> {
+          if (Platform.OS == 'android'){
+            item.bannerImg = item.bannerImg.replace('website-catalogue.ddev.site', '10.0.2.2:32775');
+          }
+
+          console.log(item.bannerImg);
+
+          return <ClothingItem
+            id={item.id}
+            title={item.title}
+            brand={item.brand}
+            intro={item.intro}
+            banner={item.bannerImg}
+            navigation={navigation}
+            onSelectedClothing={(selectedId) => { navigation.navigate('Details', { id: selectedId }) }}
+          />
+        }}
+      />
     </View>
   );
 }
@@ -19,5 +83,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  list: {
+    height: "90%",
+  },
+  title: {
+    fontSize: 24,
+    color: "#96C6F3",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    marginBottom: 8,
+    textAlign: "center"
+  }
 });
-export default CothingScreen;
+export default ClothingScreen;
